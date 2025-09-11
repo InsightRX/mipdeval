27a01e5aef4a5f2028716a21a03c715a66fb44b6
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2025-09-11 16:41:41 -0700

    add blank file for generating proseval reference data

---
+++ data-raw/proseval_pkvancothomson.R
@@ -0,0 +1,6 @@
+## code to prepare `proseval_pkvancothomson` dataset goes here
+
+# TODO: Add the code used to generate proseval reference data set here. Can use
+# `system()` or `system2()` calls for proseval
+
+usethis::use_data(proseval_pkvancothomson, overwrite = TRUE)
